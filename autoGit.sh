6256c5737e02dd4c626782203ede92aa9e61def7
autoShell <hellohfy@gmail.com>
2017-04-20 13:36:19 +0800

    2017年04月20日13:36:19脚本自动提交

--- autoGit.sh
+++ autoGit.sh
@@ -70,7 +70,9 @@ git_commit() {
 # 推送
 git_push() {
     if !${DEBUG}; then 
+        if [ -z "`git config remote.origin.url`" ]; then
         git remote add origin git@github.com:hello--world/AutoGit.git
+        fi
         git push -u origin master
     fi
 }
